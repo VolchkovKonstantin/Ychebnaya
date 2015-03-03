98d7ba8f5ad5ff6080a3b39212eb049c97d35ff9
Xaker <xaker_1@mail.ru>
2015-03-03 20:49:13 +0300

    script

---
+++ Ychebnaya/Ychebnaya/Chat/dynamics.js
@@ -0,0 +1,6 @@
+function getText() {
+    document.getElementById('nameLogin').innerHTML=document.forms['login'].elements['username'].value
+}
+function getLogin() {
+    document.getElementById('text').innerHTML=document.forms['formMsg'].elements['msg'].value
+}
