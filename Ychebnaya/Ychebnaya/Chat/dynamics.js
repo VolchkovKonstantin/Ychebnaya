53e5ad0201ea2cfc84aa2498ae1a165d6f4b8c04
Xaker <xaker_1@mail.ru>
2015-03-12 00:46:55 +0300

    Evrithing without Local Storage

--- Ychebnaya/Ychebnaya/Chat/dynamics.js
+++ Ychebnaya/Ychebnaya/Chat/dynamics.js
@@ -1,75 +1,60 @@
 var number = 0;
 function run() {
-var Container = document.getElementsByClassName('authorization')[0];
+    var Container = document.getElementsByClassName('container')[0];
     Container.addEventListener('click', delegateEvent);
 }
 function delegateEvent(event) {
-if(event.type == 'click'&& event.target.classList.contains('btn-info')) {
+    if (event.type == 'click' && event.target.classList.contains('btn-info')) {
         buttonClick();
     }
-    if(event.type == 'click'&& event.target.classList.contains('btn-success')) {
+    if (event.type == 'click' && event.target.classList.contains('btn-success')) {
         PickLogin();
     }
-    if(event.type == 'click' && event.target.classList.contains('btn-warning')) {
+    if (event.type == 'click' && event.target.classList.contains('btn-warning')) {
         changeClick(event.target.parentNode);
     }
-    if(event.type == 'click' && event.target.classList.contains('btn-danger')) {
+    if (event.type == 'click' && event.target.classList.contains('btn-danger')) {
         deleteClick(event.target.parentNode);
     }
 }
-/*function mouseOver(event)
-{
-    document.getElementById("button").src="../images/button2.png";
-}
-function mouseOut()
-{
-    document.getElementById("button").src="../images/button1.png";
-}*/
 function PickLogin() {
     var login = document.getElementById('Login');
-    document.getElementById('nameLogin').innerHTML=login.value;
+    document.getElementById('nameLogin').innerHTML = login.value;
     login.value = '';
 }
 function buttonClick() {
-    /*var radioNew = document.getElementById('clickNew');
-    var radioChange = document.getElementById('clickChange');*/
     var message = document.getElementById('newMessage');
     var user = document.getElementById('nameLogin');
     user.value = document.getElementById('nameLogin').innerHTML;
-    /*if (radioNew.checked && number == -1) {*/
-            if (user.value.localeCompare("") == 0) {
-                alert("Заполни логин!!!")
-                return;
-            }
-        if (!message.value)
-            return;
-        var item = createDiv(message.value, user.value);
-        var items = document.getElementsByClassName('items')[0];
-       // }
- /*   if (radioChange.checked) {
-            if (!message.value) {
-                return;
-            }*/
-    if(number) {
-        items.childNodes[number].appendChild(document.createTextNode(message.value));
+    if (user.value.localeCompare("") == 0) {
+        alert("Заполни логин!!!")
+        return;
+    }
+    if (!message.value)
+        return;
+    var item = createDiv(message.value, user.value);
+    var items = document.getElementsByClassName('items')[0];
+    if (number) {
+        items.childNodes[number].insertBefore(document.createTextNode(message.value), items.childNodes[number].childNodes[2]);
     }
     else {
         items.appendChild(item);
     }
-        number = 0;
-        message.value = '';
-        //}
+    number = 0;
+    message.value = '';
 }
-function deleteClick (item) {
+function deleteClick(item) {
     var items = document.getElementsByClassName('items')[0];
-    var i=0;
-    for( i = 0; i < items.childNodes.length; i++)
-        if( items.childNodes[i] === item) {
+    var i = 0;
+    for (i = 0; i < items.childNodes.length; i++)
+        if (items.childNodes[i] === item) {
             break;
         }
+    if (i < number)
+        number--;
     items.removeChild(items.childNodes[i]);
 }
-function changeClick (item) {
+function changeClick(item) {
     var b = item.childNodes[2].textContent;
     document.getElementById('newMessage').value = b;
     deletemessage(item);
@@ -77,34 +62,31 @@ function changeClick (item) {
 function deletemessage(item) {
     var items = document.getElementsByClassName('items')[0];
     var element, i = 0;
-    for(i = 0; i < items.childNodes.length; i++)
-        if(items.childNodes[i] === item) {
+    for (i = 0; i < items.childNodes.length; i++)
+        if (items.childNodes[i] === item) {
             element = i;
             number = i;
             break;
         }
     items.childNodes[element].removeChild(items.childNodes[element].childNodes[2]);
 }
-function createDiv(text, user)
-{
+function createDiv(text, user) {
     var message = document.createTextNode(text);
     var newDiv = document.createElement('div');
-    newDiv.className =  'message';
-    newDiv.appendChild(document.createTextNode(user+':'));
+    newDiv.className = 'message';
+    newDiv.appendChild(document.createTextNode(user + ':'));
     newDiv.appendChild(document.createElement('br'));
     newDiv.appendChild(message);
     newDiv.appendChild(document.createElement('br'));
-    var d = document.createElement('input');
-    d.className= 'btn  btn-danger btn-mini';
-    d.type = 'button';
-    d.name = "delete";
-    d.value = "delete";
-    newDiv.appendChild(d);
-    d = document.createElement('input');
-    d.className = 'btn btn-warning btn-mini';
-    d.name = "change";
-    d.value = "change";
-    d.type = "button";
-    newDiv.appendChild(d);
+    var btn = document.createElement('input');
+    btn.className = 'btn  btn-danger btn-mini';
+    btn.type = 'button';
+    btn.value = "delete";
+    newDiv.appendChild(btn);
+    btn = document.createElement('input');
+    btn.className = 'btn btn-warning btn-mini';
+    btn.value = "change";
+    btn.type = "button";
+    newDiv.appendChild(btn);
     return newDiv;
 }
